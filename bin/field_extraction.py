d1b71a93a0c7b0fa517335a80d23b56a24e36c0a
Randazzo <Luke.Randazzo@travelport.com>
2019-08-01 13:27:07 -0600

    initial commit

---
+++ bin/field_extraction.py
@@ -0,0 +1,141 @@
+import logging
+
+from gensim.utils import simple_preprocess
+
+from bin import lib
+from spacy.matcher import Matcher
+import re
+
+EMAIL_REGEX = r"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}"
+PHONE_REGEX = r"\(?(\d{3})?\)?[\s\.-]{0,2}?(\d{3})[\s\.-]{0,2}(\d{4})"
+
+
+def candidate_name_extractor(input_string, nlp):
+
+    doc = nlp(input_string)
+
+    # Extract entities
+    doc_entities = doc.ents
+
+    # Subset to person type entities
+    doc_persons = filter(lambda x: x.label_ == 'PERSON', doc_entities)
+    doc_persons = filter(lambda x: len(x.text.strip().split()) >= 2, doc_persons)
+    doc_persons = map(lambda x: x.text.strip(), doc_persons)
+    doc_persons = list(doc_persons)
+
+    # Assuming that the first Person entity with more than two tokens is the candidate's name
+    if len(doc_persons) > 0:
+        return doc_persons[0]
+    return "NOT FOUND"
+
+def university_extractor(input_string, nlp):
+    #Remove unnecessary spaces from file
+    input_string = re.sub(' +', ' ', input_string)
+
+    doc = nlp(input_string)
+    
+    doc_propernouns = filter(lambda x: x.label_ == 'ORG', doc.ents)
+    doc_propernouns = map(lambda x: x, doc_propernouns)
+    doc_propernouns = list(doc_propernouns)
+    doc_universities = list()
+    for x in doc_propernouns: 
+        for y in x.text.strip().split('\n'):
+            doc_universities.append(y) 
+
+    doc_universities = filter(lambda x: 'University' in x or 'College' in x, doc_universities)
+    doc_universities = list(doc_universities)
+    doc_universities = keep_unique(doc_universities)
+
+    if len(doc_universities) > 0:
+        return doc_universities
+    return ""
+
+patterna = [{"POS": "PROPN","OP":"*"}, {"POS": "PROPN","OP":"*"}, 
+            {"POS": "PROPN"}, {"TEXT": {"REGEX": "[Mm]ajor"}}]
+patternb = [{"TEXT": {"REGEX": "[Mm]ajor(ed)?"}}, {"TEXT": {"REGEX": ": ?"}}, {"POS": "PROPN","OP":"*"}, 
+            {"POS": "PROPN","OP":"*"}, {"POS": "PROPN"}]
+patternc = [{"TEXT": {"REGEX": "([Bb]achelor('s)?|[Mm]aster('s)?)"}}, {"TEXT": {"REGEX": "(of|in)"}}, 
+            {"TEXT": {"REGEX": "(Sciences?|Arts?)"}, "OP": "*"},
+            {"TEXT": {"REGEX": "(of|in)"}, "OP": "*"}, {"POS": "PROPN", "OP":"*"},{"POS": "PROPN", "OP":"*"},{"POS": "PROPN", "OP":"*"}]
+patternd = [{"TEXT": {"REGEX": "(B|M)\.?(S|A)\.?"}}, {"TEXT": {"REGEX": "(,|:|in|of|[Dd]egree)"}}, {"TEXT": {"REGEX": "(in|of)"}, "OP":"*"},
+            {"POS":"PROPN", "OP":"*"},{"POS":"PROPN", "OP":"*"},{"POS":"PROPN"}]
+
+def major_extractor(input_string, nlp):
+    #Remove unnecessary spaces from file
+    input_string = re.sub(' +', ' ', input_string)
+
+    doc = nlp(input_string)
+
+    matcher = Matcher(nlp.vocab)
+    matcher.add("Major", None, patterna)
+    matcher.add("Majorb", None, patternb)
+    matcher.add("Degreea", None, patternc)
+    matcher.add("Degreeb", None, patternd)
+    matches = matcher(doc)
+
+    out_m = list()
+    for id, start, end in matches:
+        out_m.append(doc[start:end].text)
+    out_m = keep_unique(out_m)
+    print('testing matches:\n')
+    print(out_m)
+
+    if len(out_m) > 0:
+        return out_m
+    return ""
+
+def extract_fields(df):
+    for extractor, items_of_interest in lib.get_conf('extractors').items():
+        df[extractor] = df['text'].apply(lambda x: extract_skills(x, extractor, items_of_interest))
+    return df
+
+
+def extract_skills(resume_text, extractor, items_of_interest):
+    potential_skills_dict = dict()
+    matched_skills = set()
+
+    # TODO This skill input formatting could happen once per run, instead of once per observation.
+    for skill_input in items_of_interest:
+
+        # Format list inputs
+        if type(skill_input) is list and len(skill_input) >= 1:
+            potential_skills_dict[skill_input[0]] = skill_input
+
+        # Format string inputs
+        elif type(skill_input) is str:
+            potential_skills_dict[skill_input] = [skill_input]
+        else:
+            logging.warn('Unknown skill listing type: {}. Please format as either a single string or a list of strings'
+                         ''.format(skill_input))
+
+    for (skill_name, skill_alias_list) in potential_skills_dict.items():
+
+        skill_matches = 0
+        # Iterate through aliases
+        for skill_alias in skill_alias_list:
+            # Add the number of matches for each alias
+            skill_matches += lib.term_count(resume_text, skill_alias.lower())
+
+        # If at least one alias is found, add skill name to set of skills
+        if skill_matches > 0:
+            matched_skills.add(skill_name)
+
+    return matched_skills
+
+def keep_unique(lst):
+    unique_list = list()
+
+    for string in lst:
+        add = True
+        string = re.sub(' +', ' ', string)
+        string = string.strip()
+        for xstring in unique_list:
+            if string in xstring:
+                add = False
+            elif xstring in string:
+                unique_list.remove(xstring)
+        if add:
+            unique_list.append(string)
+    
+    return unique_list
+
